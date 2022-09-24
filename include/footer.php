a6cac88137386d05a52d2adea41350580e709658
DevFullRLD <rildoclaro@outlook.com>
2022-09-24 12:08:25 -0300

    alterações nos include

---
+++ include/footer.php
@@ -0,0 +1,49 @@
+<div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-light mb-4">Endereço</h4>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Rua: Estrela guia, N° 282, Hortolândia-SP</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+55 19 99745-2533</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>rivimotos@outlook.com</p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-light mb-4">Horario de Abertura</h4>
+                <h6 class="text-light">Segunda - Sexta</h6>
+                <p class="mb-4">9h - 18h</p>
+                <h6 class="text-light">Sabado</h6>
+                <p class="mb-0">9h - 12h</p>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-light mb-4">Serviços</h4>
+                <a class="btn btn-link" href="">Testes de Diagnóstico</a>
+                <a class="btn btn-link" href="">Revisões</a>
+                <a class="btn btn-link" href="">Montadoras Suportadas</a>
+                <a class="btn btn-link" href="">Tipos de Oleo</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-light mb-4">Contate-nos</h4>
+                <p>Para mais informações ou até mesmo um atendimento direcionado basta ligar!!</p>
+                <div class="position-relative mx-auto" style="max-width: 400px;">
+                    <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Digite seu Email">
+                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="copyright">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="border-bottom" href="#">Alex Preparações</a>, Todos os Direitos Reservados.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ include/sobre.php
@@ -0,0 +1,56 @@
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6 pt-4" style="min-height: 400px;">
+                <div class="position-relative h-100 wow fadeIn" data-wow-delay="0.1s">
+                    <img class="position-absolute img-fluid w-100 h-100" src="img/about.jpg" style="object-fit: cover;" alt="">
+                    <div class="position-absolute top-0 end-0 mt-n4 me-n4 py-4 px-5" style="background: rgba(0, 0, 0, .08);">
+                        <h1 class="display-4 text-white mb-0">4 <span class="fs-4">Anos de</span></h1>
+                        <h4 class="text-white">Experiência</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h6 class="text-primary text-uppercase">Sobre Nós</h6>
+                <h3 class="mb-4"><span class="text-primary">Alex Preparações</span> Uma Oficina familiar onde buscamos inovar e sempre estar atualizado de acordo com o mercado</h3>
+                <p class="mb-4"> Oficina conta com as melhores tecnologias do mercado incluindo: Reprogramação de centrais, correção de erros etc...</p>
+                <div class="row g-4 mb-3 pb-3">
+                    <div class="col-12 wow fadeIn" data-wow-delay="0.1s">
+                        <div class="d-flex">
+                            <div class="bg-light d-flex flex-shrink-0 align-items-center justify-content-center mt-1" style="width: 45px; height: 45px;">
+                                <span class="fw-bold text-secondary">01</span>
+                            </div>
+                            <div class="ps-3">
+                                <h6>Correção de Falhas</h6>
+                                <span>Correção de falhas na injeção eletrônica</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
+                        <div class="d-flex">
+                            <div class="bg-light d-flex flex-shrink-0 align-items-center justify-content-center mt-1" style="width: 45px; height: 45px;">
+                                <span class="fw-bold text-secondary">02</span>
+                            </div>
+                            <div class="ps-3">
+                                <h6>Reprogramação de Centrais</h6>
+                                <span>Reprogramação de centrais para melhorar fluxo de combstivel</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 wow fadeIn" data-wow-delay="0.5s">
+                        <div class="d-flex">
+                            <div class="bg-light d-flex flex-shrink-0 align-items-center justify-content-center mt-1" style="width: 45px; height: 45px;">
+                                <span class="fw-bold text-secondary">03</span>
+                            </div>
+                            <div class="ps-3">
+                                <h6>Remap</h6>
+                                <span>Remapeamento de motos trazendo performance</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="" class="btn btn-primary py-3 px-5">Quer saber mais ? Clique!<i class="fa fa-arrow-right ms-3"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
