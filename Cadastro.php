3364cd7cbf87032f77f13d545e5acb43ecee84ff
DevFullRLD <rildoclaro@outlook.com>
2022-10-28 22:44:19 -0300

    Iniciando CRUD

---
+++ Cadastro.php
@@ -0,0 +1,100 @@
+<?php
+    //Instancia do Banco de dados
+    require_once "MySQL/ConfigSession.php";
+    require_once "MySQL/conexao.php";
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Alex Preparações</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600;700&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet"> 
+    <!-- Bootstrap da pagina -->
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <?php
+        require_once "include/funcao/validaForm.php";
+    ?>
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Topbar Start -->
+    <?php
+        require_once "include/topbar.php";
+    ?>  
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <?php
+        if (isset($_SESSION['id'])) {
+            require_once "include/menuLogado.php";
+        } else {
+            require_once "include/menu.php";
+        }
+    ?>
+    <!-- Navbar End -->
+
+    <!-- Formulario Start -->
+    <?php
+        require_once "include/FrmCadastro.php";
+    ?> 
+    <!-- Formulario End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ include/modal.php
@@ -0,0 +1,21 @@
+<!-- Modal Para Escolher tipo de Cadastro -->
+<div id="bg"></div>
+<div class="box">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            </div>
+            <div class="modal-body" style="text-align:center;">
+                <h4 class="modal-title" style="text-align:center;">O Que deseja efetuar ?</h4>
+                <br>
+                <a class="btn btn-primary" href="Cadastro.php" role="button">Cadastrar-se</a> <br>
+                <a class="btn btnbtn-primary" style="margin-top:10px;" href="Login.php" role="button">Logar</a>
+                <br><br>
+            </div>
+            <div class="modal-footer" style="text-align:center;">
+                <p>&copy;Alex Preparações. Since 2017&trade;<p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
