640595a5497113abecbfc193e608e7afa5a3c79d
DevFullRLD <rildoclaro@outlook.com>
2023-04-24 20:18:21 -0300

    Correção de montadoras, foram inseridas: - bmw - dafra - suziki - yamaha - honda - kawasaki - ktm - triumph - harley-davidson

--- include/montadoras.php
+++ include/montadoras.php
@@ -54,4 +54,34 @@
       </div>
     </div>
   </div>
+  <br>
+  <div class="card-deck">
+    <div class="card">
+    <br><br><br>
+      <img src="img/triumph.png" class="card-img-top" alt="TRIUMPH">
+      <div class="card-body" align="center">
+      <br><br><br>
+        <h5 class="card-title" >TRIUMPH</h5>
+        <a href="#" class="btn btn-primary" style="border-radius: 100px;">Visualizar Manutenções</a>
+      </div>
+    </div>
+    <div class="card">
+      <br><br><br>
+      <img src="img/harley.png" class="card-img-top" alt="Harley-Davidson">
+      <div class="card-body" align="center">
+      <br><br><br>
+        <h5 class="card-title" >Harley-Davidson</h5>
+        <a href="#" class="btn btn-primary" style="border-radius: 100px;">Visualizar Manutenções</a>
+      </div>
+    </div>
+    <div class="card">
+      <br>
+      <img src="img/ktm.png" class="card-img-top" alt="KTM">
+      <div class="card-body" align="center">
+      <br><br>
+        <h5 class="card-title" >KTM</h5>
+        <a href="#" class="btn btn-primary" style="border-radius: 100px;">Visualizar Manutenções</a>
+      </div>
+    </div>
+  </div>
 </div>
