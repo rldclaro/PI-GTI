82129f05f3004dd1d87eca156e0e2511ed8d0631
DevFullRLD <rildoclaro@outlook.com>
2023-04-24 19:51:40 -0300

    Correção de montadoras, foram inseridas: - bmw - dafra - suziki - yamaha - honda - kawasaki

--- include/montadoras.php
+++ include/montadoras.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="card-deck">
     <div class="card">
-      <img src="img/yamaha.png" class="card-img-top" alt="Imagem do card 1">
+      <img src="img/yamaha.png" class="card-img-top" alt="YAMAHA">
       <div class="card-body" align="center">
     <br>
         <h5 class="card-title" >YAMAHA</h5>
@@ -10,7 +10,7 @@
     </div>
     <div class="card">
     <br><br>
-      <img src="img/honda.png" class="card-img-top" alt="Imagem do card 2">
+      <img src="img/honda.png" class="card-img-top" alt="HONDA">
       <div class="card-body" align="center">
     <br><br>
         <h5 class="card-title" >HONDA</h5>
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div class="card">
-      <img src="img/kawasaki.png" class="card-img-top" alt="Imagem do card 2">
+      <img src="img/kawasaki.png" class="card-img-top" alt="KAWASAKI">
       <div class="card-body" align="center">
       <br><br>
         <h5 class="card-title" >KAWASAKI</h5>
@@ -29,27 +29,28 @@
   <br>
   <div class="card-deck">
     <div class="card">
-      <img src="img/kawasaki.png" class="card-img-top" alt="Imagem do card 1">
-      <div class="card-body">
-        <h5 class="card-title">Título do card 1</h5>
-        <p class="card-text">Descrição do card 1.</p>
-        <a href="#" class="btn btn-primary">Botão 1</a>
+      <img src="img/suzuki.png" class="card-img-top" alt="SUZUKI">
+      <div class="card-body" align="center">
+      <br><br>
+        <h5 class="card-title" >SUZUKI</h5>
+        <a href="#" class="btn btn-primary" style="border-radius: 100px;">Visualizar Manutenções</a>
       </div>
     </div>
     <div class="card">
-      <img src="img/kawasaki.png" class="card-img-top" alt="Imagem do card 2">
-      <div class="card-body">
-        <h5 class="card-title">Título do card 2</h5>
-        <p class="card-text">Descrição do card 2.</p>
-        <a href="#" class="btn btn-primary">Botão 2</a>
+      <img src="img/bmw.png" class="card-img-top" alt="BMW">
+      <div class="card-body" align="center">
+      <br><br>
+        <h5 class="card-title" >BMW</h5>
+        <a href="#" class="btn btn-primary" style="border-radius: 100px;">Visualizar Manutenções</a>
       </div>
     </div>
     <div class="card">
-      <img src="img/kawasaki.png" class="card-img-top" alt="Imagem do card 3">
-      <div class="card-body">
-        <h5 class="card-title">Título do card 3</h5>
-        <p class="card-text">Descrição do card 3.</p>
-        <a href="#" class="btn btn-primary">Botão 3</a>
+      <br>
+      <img src="img/dafra.png" class="card-img-top" alt="DAFRA">
+      <div class="card-body" align="center">
+      <br><br>
+        <h5 class="card-title" >DAFRA</h5>
+        <a href="#" class="btn btn-primary" style="border-radius: 100px;">Visualizar Manutenções</a>
       </div>
     </div>
   </div>
